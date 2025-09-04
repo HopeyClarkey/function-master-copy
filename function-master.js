bd86b77b51664511f04fc6a7f99495155a0a576c
Hope Clarke <thehopeclarke@gmail.com>
2025-09-03 20:27:19 -0500

    added psudeocode

--- function-master.js
+++ function-master.js
@@ -14,7 +14,7 @@ function objectValues(object) {
 function keysToString(object) {
     // Should take an object and return all its string values separated 
     // by a space
-        var tempArr = object.values(object);
+        var tempArr = object.keys(object);
         tempArr.join(' ');
 }
 
@@ -23,7 +23,10 @@ function keysToString(object) {
 //////////////////////////////////////////////////////////////////////
 
 function valuesToString(object) {
-    
+    //Should take an object and return all its string values in a string 
+    //each separated with a space"
+    var tempArr = object.values(object);
+    tempArr.join(' ');
 }
 
 //////////////////////////////////////////////////////////////////////
@@ -31,7 +34,8 @@ function valuesToString(object) {
 //////////////////////////////////////////////////////////////////////
 
 function arrayOrObject(collection) {
-    
+    //Should take one argument and return 'array' if its an array and 
+    //'object' if its an object"
 }
 
 //////////////////////////////////////////////////////////////////////
@@ -39,7 +43,7 @@ function arrayOrObject(collection) {
 //////////////////////////////////////////////////////////////////////
 
 function capitalizeWord(string) {
-    
+    //Should take a string of one word, and return the word with its first letter capitalized
 }
 
 //////////////////////////////////////////////////////////////////////
@@ -47,7 +51,7 @@ function capitalizeWord(string) {
 //////////////////////////////////////////////////////////////////////
 
 function capitalizeAllWords(string) {
-    
+    //Should take a string of words and return a string with all the words capitalized
 }
 
 //////////////////////////////////////////////////////////////////////
@@ -55,7 +59,7 @@ function capitalizeAllWords(string) {
 //////////////////////////////////////////////////////////////////////
 
 function welcomeMessage(object) {
-
+    //Should take an object with a name property and return 'Welcome <Name>!'
 }
 
 //////////////////////////////////////////////////////////////////////
@@ -63,7 +67,7 @@ function welcomeMessage(object) {
 //////////////////////////////////////////////////////////////////////
 
 function profileInfo(object) {
-
+    //Should take an object with a name an a species and return '<Name> is a <Species>
 }
 
 //////////////////////////////////////////////////////////////////////
@@ -71,7 +75,8 @@ function profileInfo(object) {
 //////////////////////////////////////////////////////////////////////
 
 function maybeNoises(object) {
-
+    //Should take an object, if this object has a noises array return 
+    //them as a string separated by a space, if there are no noises return 'there are no noises'
 }
 
 //////////////////////////////////////////////////////////////////////
@@ -79,7 +84,8 @@ function maybeNoises(object) {
 //////////////////////////////////////////////////////////////////////
 
 function hasWord(string, word) {
-
+    //Should take a string of words and a word and return true if <word> 
+    //is in <string of words>, otherwise return false.
 }
 
 //////////////////////////////////////////////////////////////////////
@@ -87,7 +93,8 @@ function hasWord(string, word) {
 //////////////////////////////////////////////////////////////////////
 
 function addFriend (name, object) {
-
+    //Should take a name and an object and add the name to the object's
+    // friends array then return the object
 }
 
 //////////////////////////////////////////////////////////////////////
@@ -95,7 +102,8 @@ function addFriend (name, object) {
 //////////////////////////////////////////////////////////////////////
 
 function isFriend(name, object) {
-
+    //Should take a name and an object and return true if <name> is a 
+    //friend of <object> and false otherwise"
 }
 
 //////////////////////////////////////////////////////////////////////
@@ -103,7 +111,8 @@ function isFriend(name, object) {
 //////////////////////////////////////////////////////////////////////
 
 function nonFriends(name, array) {
-
+    //Should take a name and a list of people, and return a list of 
+    //all the names that <name> is not friends with
 }
 
 //////////////////////////////////////////////////////////////////////
@@ -111,7 +120,9 @@ function nonFriends(name, array) {
 //////////////////////////////////////////////////////////////////////
 
 function updateObject(object, key, value) {
-
+    // Should take an object, a key and a value. Should update the 
+    // property <key> on <object> with new <value>. If <key> does not 
+    // exist on <object> create it.
 }
 
 //////////////////////////////////////////////////////////////////////
@@ -119,7 +130,8 @@ function updateObject(object, key, value) {
 //////////////////////////////////////////////////////////////////////
 
 function removeProperties(object, array) {
-
+    // Should take an object and an array of strings. Should remove 
+    // any properties on <object> that are listed in <array>
 }
 
 //////////////////////////////////////////////////////////////////////
@@ -127,7 +139,7 @@ function removeProperties(object, array) {
 //////////////////////////////////////////////////////////////////////
 
 function dedup(array) {
-
+    //Should take an array and return an array with all the duplicates removed"
 }
 
 //////////////////////////////////////////////////////////////////////
